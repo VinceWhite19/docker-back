118ab40ee89b6eac5d2804a4a358562ce545dfb0
Vince <v.yavorskiy@mdrecruitment.ua>
2020-01-31 17:14:28 +0200

    sql error fix

--- migrations/2-data.sql
+++ migrations/2-data.sql
@@ -6,7 +6,7 @@ INSERT INTO employees (first_name, second_name, description, office_id) VALUES
 ('Вася', 'Пупкин', 'официант', 1),
 ('Петя', 'Иванов', 'админ', 2),
 ('Петя', 'Иванов', 'официант', 2);
-INSERT INTO reviews (rating, description, customer_first_name, customer_second_name, email, phone, parent_table_id) VALUES
+INSERT INTO reviews (rating, description, customer_first_name, customer_second_name, email, phone, employee_id) VALUES
 (5, 'Супер-сервис спасибо!!!', 'John', 'Doe', 'joe@test.com', 0123456789, 1),
 (4, 'Все гуд', 'Bob', 'Smith', 'Bob@smith.com', 0123456789, 1),
 (3, 'Ну такое...', 'Василь', 'Петрович', 'vasil@test.com', 0123456789, 2);
